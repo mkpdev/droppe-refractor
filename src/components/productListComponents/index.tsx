b75b283bb5b956a3d798b244aa8ea114df1a0a9a
mkpdev <mukpdev11@gmail.com>
2022-06-09 15:32:16 +0530

    optimize productListComponent & update target in tsconfig

--- src/components/productListComponents/index.tsx
+++ src/components/productListComponents/index.tsx
@@ -2,21 +2,22 @@ import * as React from "react";
 import lodash from 'lodash'
 import { FaStar } from "react-icons/fa";
 import styles from "./product-list-components.module.css";
+import { Product as ProductInterface } from "../../model";
 
 interface IPostsProps {
-  products: any;
+  products: ProductInterface[];
   onFav: (title: string) => void;
 }
 
-
 export default class Posts extends React.Component<IPostsProps, {}> {
-  constructor(props: any) { super(props) }
-  render(){
+  constructor(props: IPostsProps) { super(props) }
+  render() {
+    const { products, onFav } = this.props;
     let productsarr = []
-      for (const [i, p] of this.props.products.entries()) {
-        productsarr.push(
-          <Product key={i} index={i} product={p} onFav={this.props.onFav} />
-        );
+    for (const [i, p] of products.entries()) {
+      productsarr.push(
+        <Product key={p.id} index={i} product={p} onFav={onFav} />
+      );
     }
     return <div>{lodash.reverse(productsarr)}</div>
   }
@@ -24,14 +25,14 @@ export default class Posts extends React.Component<IPostsProps, {}> {
 
 export const Product: React.FC<{
   index: number;
-  product: { title: string; description: string; price: number; isFavorite: boolean; rating: {rate: number; count: number} };
+  product: ProductInterface;
   onFav: (title: string) => void;
 }> = ({ product, onFav }) => {
-  const {product: productClass, productBody, actionBarItem, actionBarItemLabel} = styles
-  // Problem: Now product title can be too long, I just put overflowX as fix now
+  const { product: productClass, productBody, actionBarItem, actionBarItemLabel } = styles
+
   return (
-    <span className={productClass} style={{display: 'inline-block', overflowX: 'scroll', float: 'none', clear: 'both'}}>
-      <span className={styles['product-title']} style={{overflowX: 'hidden'}}>{product.title}</span>
+    <span className={productClass} style={{ display: 'inline-block', float: 'none', clear: 'both' }}>
+      <div className={styles['product-title']} style={{ overflow: 'auto', textOverflow: 'ellipsis' }}>{product.title}</div>
 
       <p><strong>Rating: {product.rating ? `${product.rating.rate}/5` : ''}</strong></p>
 
@@ -39,18 +40,17 @@ export const Product: React.FC<{
 
       <p className={productBody}>
         <span><b>Description:</b></span>
-        <br/>
+        <br />
         {product.description}
-     </p>
+      </p>
 
-      <span className={styles['action_bar']} style={{display: 'table', width: "100%"}}>
+      <span className={styles['action_bar']} style={{ display: 'table', width: "100%" }}>
         <span
-          className={`${actionBarItem} ${
-            product.isFavorite ? "active" : ""
-          }`}
+          className={`${actionBarItem} ${product.isFavorite ? "active" : ""
+            }`}
           role="button"
           onClick={() => {
-              onFav(product.title);
+            onFav(product.title);
           }}
         >
           <FaStar /> <span className={actionBarItemLabel}>{!!(!!(product.isFavorite)) ? 'Remove from favorites' : 'Add to favorites'}</span>
